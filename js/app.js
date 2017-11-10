91b2f2f45acda125f122c19b91149f397aa03280
betsy18 <betsy_dessire@hotmail.com>
2017-11-10 02:25:45 -0500

    Editando el archivo .js, css y agregando el readme

--- js/app.js
+++ js/app.js
@@ -1,19 +1,49 @@
-// se ejecuta cuando el obj, este totalmente cargado la pag. web
-window.onload = function() {
-    //escoger solo el td de nro 6
-    var oneElement = document.getElementsByTagName('td')[6];
-    oneElement.addEventListener('click', addBgc);
+// keyup
 
-    var oneColor = document.getElementsByTagName('td');
-    //oneColor.addEventListener('onekeydown', press);
-};
-   
-function addBgc(event){
-    event.target.style.backgroundColor='red';
-}
+window.addEventListener('keyup', function(press) {
 
-//onkeydown
-
-function press() {
-    event.target.style.backgroundColor = "blue";
-}
+  var ascii = press.keyCode;
+  var oneElement = document.getElementsByTagName('td')[6];
+  var moveElement = document.getElementsByTagName('td');
+  // Se ejecuta al presionar la tecla intro
+  if (ascii === 13) {
+    oneElement.classList.add('red');
+    moveElement[2].classList.remove('red');
+    moveElement[10].classList.remove('red');
+    moveElement[5].classList.remove('red');
+    moveElement[7].classList.remove('red');
+  }
+  // flecha arriba
+  if (ascii === 38) {
+    moveElement[2].classList.add('red');
+    moveElement[6].classList.remove('red');
+    moveElement[10].classList.remove('red');
+    moveElement[5].classList.remove('red');
+    moveElement[7].classList.remove('red');
+  }
+  // flecha abajo
+  if (ascii === 40) {
+    moveElement[10].classList.add('red');
+    moveElement[2].classList.remove('red');
+    moveElement[6].classList.remove('red');
+    moveElement[5].classList.remove('red');
+    moveElement[7].classList.remove('red');
+  }
+  // flecha izquierda
+  if (ascii === 37) {
+    moveElement[5].classList.add('red');
+    moveElement[10].classList.remove('red');
+    moveElement[2].classList.remove('red');
+    moveElement[6].classList.remove('red');
+    moveElement[7].classList.remove('red');
+  }
+  // flecha derecha
+  if (ascii === 39) {
+    moveElement[7].classList.add('red');
+    moveElement[5].classList.remove('red');
+    moveElement[2].classList.remove('red');
+    moveElement[6].classList.remove('red');
+    moveElement[10].classList.remove('red');
+  }
+});
+  
